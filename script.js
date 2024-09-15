4dacae023af448ca6e120808f1b943e05df52976
borlingball15 <ananya.borle@gmail.com>
2024-09-15 11:44:53 -0400

    changes 2

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-document.getElementById('slider').addEventListener('click', function() {
-   const body = document.body;
-    const sliderCircle = document.getElementById('sliderCircle');
-    const sliderText = document.getElementById('sliderText');
+// document.getElementById('slider').addEventListener('click', function() {
+//    const body = document.body;
+//     const sliderCircle = document.getElementById('sliderCircle');
+//     const sliderText = document.getElementById('sliderText');
 
-    if (body.style.backgroundColor === 'black') {
-        body.style.backgroundColor = 'white';
-        sliderCircle.style.left = '2px';
-        sliderCircle.style.backgroundColor = '#fff';
-        sliderText.style.left = '20px';
-        sliderText.style.opacity = '0';
+//     if (body.style.backgroundColor === 'black') {
+//         body.style.backgroundColor = 'white';
+//         sliderCircle.style.left = '2px';
+//         sliderCircle.style.backgroundColor = '#fff';
+//         sliderText.style.left = '20px';
+//         sliderText.style.opacity = '0';
+//     } else {
+//         body.style.backgroundColor = 'black';
+//         sliderCircle.style.left = 'calc(100% - 98px)';
+//         sliderCircle.style.backgroundColor = 'pink';
+//         sliderText.style.left = 'calc(100% - 200px)';
+//         sliderText.style.opacity = '1';
+//     }
+// });
+
+const slider = document.querySelector(".slider");
+const sliderText = document.querySelector(".slider-text");
+const questionMark = document.querySelector(".question-mark");
+
+slider.addEventListener("click", function () {
+    // slider class
+    slider.classList.toggle("active");
+    document.body.classList.toggle("active");
+
+    if (slider.classList.contains("active")) {
+        // name text
+        sliderText.style.opacity = 1;
+
+        // question mark 
+        questionMark.style.opacity = 1;
     } else {
-        body.style.backgroundColor = 'black';
-        sliderCircle.style.left = 'calc(100% - 98px)';
-        sliderCircle.style.backgroundColor = 'pink';
-        sliderText.style.left = 'calc(100% - 200px)';
-        sliderText.style.opacity = '1';
+        // hiding the text and q.m
+        sliderText.style.opacity = 0;
+        questionMark.style.opacity = 0;
     }
 });
